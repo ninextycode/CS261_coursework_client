acf6a56722dbdfd164dd2662b593aea915e72b5c
maxim <ninexty@gmail.com>
2018-02-17 20:33:01 +0000

    Working on news

--- frontend_wrapper/BotWrapper.py
+++ frontend_wrapper/BotWrapper.py
@@ -1,4 +1,5 @@
 import frontend_wrapper.Log as l
+import json
 
 class BotWrapper:
     instance = None
@@ -16,7 +17,11 @@ class BotWrapper:
     def set_client(self, client):
         self.socket_client = client
 
-    def send_to_server(self, json):
+    def send_text_to_server(self, message):
+        json_data = {"mime_type": "text/plain", "data": message}
+        self.send_json_to_server(json_data)
+
+    def send_json_to_server(self, json):
         self.socket_client.send(json)
 
     def on_json_from_server(self, json):
